6f856faa583028acc430d3b679e7c8ff460f4519
Denys Ivanets <denself@gmail.com>
2017-09-13 15:30:54 +0300

    Add DKIM and SPF validation. #8

--- server.py
+++ server.py
@@ -1,14 +1,46 @@
 import email
 import logging
+from email.message import Message
 
+import dkim
+import spf
 from aiosmtpd.smtp import Envelope, Session, SMTP
 
 from tools import message_to_display
 
 log = logging.getLogger("smtphandler")
 
+test_results = ['FAIL', 'PASS']
+
 
 class ExampleHandler:
+
+    def __init__(self,
+                 verify_dkim: bool = False,
+                 verify_spf: bool = False):
+        self.verify_dkim = verify_dkim
+        self.verify_spf = verify_spf
+
+    async def handle_MAIL(self,
+                          server: SMTP,
+                          session: Session,
+                          envelope: Envelope,
+                          address: str,
+                          mail_options: list):
+
+        ip = session.peer[0]
+        result, description = spf.check2(ip, address, session.host_name)
+
+        log.info("SPF: %s, %s", result, description)
+
+        if self.verify_spf and result != 'pass':
+            return '550 SPF validation failed'
+
+        envelope.mail_from = address
+        envelope.mail_options.extend(mail_options)
+
+        return '250 OK'
+
     async def handle_RCPT(self,
                           server: SMTP,
                           session: Session,
@@ -18,17 +50,26 @@ class ExampleHandler:
         # Accept all incoming mail for now
         # if not address.endswith('@example.com'):
         #     return '550 not relaying to that domain'
-        log.info("Handle RCPT for %s", address)
+        log.debug("Handle RCPT for %s", address)
         envelope.rcpt_tos.append(address)
         return '250 OK'
 
     async def handle_DATA(self,
                           server: SMTP,
                           session: Session,
-                          envelop: Envelope):
-        payload = email.message_from_bytes(envelop.content)
-        text = message_to_display(payload)
-        log.debug('Message from %s', envelop.mail_from)
-        log.debug('Message for %s', envelop.rcpt_tos)
-        log.debug('Message data:\n%s', text)
+                          envelope: Envelope):
+
+        valid_dkim = dkim.verify(envelope.content)
+        log.info("DKIM: %s", test_results[valid_dkim])
+
+        message: Message = email.message_from_bytes(envelope.content)
+
+        log.info('From: %s', message['From'])
+        log.info('To: %s', message['To'])
+        log.info('Subject: %s', message['Subject'])
+        log.info('Message data:\n%s', message_to_display(message))
+
+        if self.verify_dkim and not valid_dkim:
+            return '550 DKIM validation failed'
+
         return '250 Message accepted for delivery'
--- tests/test_server.py
+++ tests/test_server.py
@@ -10,10 +10,10 @@ from server import ExampleHandler
 class TestExampleHandler:
 
     def setup_method(self, method):
-        self.handler = ExampleHandler()
+        self.handler = ExampleHandler(verify_spf=True)
         self.envelope = Envelope()
-        self.envelope.content = b"""From: Cate Person <cate@example.com>
-To: Dave Person <dave@example.com>
+        self.envelope.content = b"""From: Cate Person <cate@gmail.com>
+To: Dave Person <dave@gmail.com>
 Subject: A test
 Message-ID: <bee>
 
@@ -21,7 +21,37 @@ Hi Dave, this is Cate."""
         self.server = SMTP(self.handler)
 
     @pytest.mark.asyncio
+    async def test_handle_MAIL_spf(self):
+        server = SMTP(self.handler)
+        session = Session(server.loop)
+        session.peer = ['209.85.220.69', 65000]
+        session.host_name = 'gmail.com'
+        response = await self.handler.handle_MAIL(
+            self.server,
+            session,
+            Envelope(),
+            'cate@gmail.com',
+            []
+        )
+        assert response == '250 OK'
+
+    @pytest.mark.asyncio
+    async def test_handle_MAIL_no_spf(self):
+        server = SMTP(self.handler)
+        session = Session(server.loop)
+        session.peer = ['127.0.0.1', 65000]
+        response = await self.handler.handle_MAIL(
+            self.server,
+            session,
+            self.envelope,
+            'cate@gmail.com',
+            []
+        )
+        assert response == '550 SPF validation failed'
+
+    @pytest.mark.asyncio
     async def test_handle_DATA(self):
+        self.handler.verify_dkim = False
         server = SMTP(self.handler)
         response = await self.handler.handle_DATA(
             self.server,
@@ -29,6 +59,17 @@ Hi Dave, this is Cate."""
             self.envelope
         )
         assert response == '250 Message accepted for delivery'
+
+    @pytest.mark.asyncio
+    async def test_handle_DATA_fail_dkim(self):
+        self.handler.verify_dkim = True
+        server = SMTP(self.handler)
+        response = await self.handler.handle_DATA(
+            self.server,
+            Session(server.loop),
+            self.envelope
+        )
+        assert response == '550 DKIM validation failed'
 
     @pytest.mark.asyncio
     async def test_handle_RCPT(self):
